2fc6d20bef54f938506df08204abc5617ad62111
AmosMichael <2313214746@qq.com>
2017-11-21 23:15:47 +0800

    re-edit related stuff

--- QuickSortLoopImplementaion.c
+++ QuickSortLoopImplementaion.c
@@ -1,16 +1,21 @@
 #include <stdio.h>
-#define N 20
+#define N 10000
 typedef int ElemType;
 int main(int argc, char const *argv[])
 {
 
-	ElemType A[N] = { 5, 3, 7, 4, 9 , 2, 10, 18, 28, -1, -3, 40, 36, 100, -9, 26, 33, 39, 34, 19}; // 20 elements
+	// ElemType A[N] = { 5, 3, 7, 4, 9 , 2, 10, 18, 28, -1, -3, 40, 36, 100, -9, 26, 33, 39, 34, 19}; // 20 elements
 									  // printf("%d\n",A[19] );
-
+	ElemType A[N];
 	ElemType st[N][2]; int top = -1;
 	int  range_start, range_end, i, j, pivot;
 	st[++top][0] = 0; st[top][1] = N - 1;
 
+
+	for(i=N-1; i>0; i--)	
+	{
+		A[i] = N-i;
+	}
 	while (top>-1) //开始快排
 	{
 		range_start = i = st[top][0]; range_end = j = st[top][1];
@@ -50,6 +55,6 @@ int main(int argc, char const *argv[])
 	}// while
 	//输出结果
 	for (i = 0; i < N; i++)
-		printf("%d ", A[i]);
+		printf("%d ", A[N-1]);
 	return 0;
 }
