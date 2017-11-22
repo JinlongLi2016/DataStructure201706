b90369e6af14152788063bfb43f34f11f606b8c6
AmosMichael <2313214746@qq.com>
2017-11-22 14:45:17 +0800

    Change two ConstructBinTree() function names to ConstructBinTreePI() and ConstructBinTreeLI(). Also change some important name errors.

--- SequenceToBinTree.c
+++ SequenceToBinTree.c
@@ -1,17 +1,20 @@
 // 将两个二叉树遍历序列 转换为 二叉树并返回
 
+#include <stdio.h>
+#include "BinTree.c"
 
 
 // 1 由先序序列和中序序列 构造二叉树
-BinTree ConstructBinTree(char preS[], psi, inS[], isi, num)
+BinTree ConstructBinTreePI(char preS[],int psi,char inS[],int isi,int num)
 {// preS[] inS[] contains the traversing sequence
 // psi: preS[] preSequence starting index while isi means inS[] starting index
 // num: the number of following elements, including(preS[psi] and inS[isi])
+	BinTree T;
 	if(num == 0)	return NULL;
 	else if(num == 1)
 	{
-		T = (BinTree)malloc(sizeof(TreeNode));
-		T->data = preS[psi]; T->Left = T->Right = NULL;
+		T = (BinTree)malloc(sizeof(struct TreeNode));
+		T->Data = preS[psi]; T->Left = T->Right = NULL;
 		return T;
 	}
 	else if(num > 1)
@@ -21,7 +24,7 @@ BinTree ConstructBinTree(char preS[], psi, inS[], isi, num)
 				break;
 		}
 		//构造根节点
-		T = (BinTree)malloc(sizeof(TreeNode));
+		T = (BinTree)malloc(sizeof(struct TreeNode));
 		T->Data = preS[psi];
 
 		//先序遍历中,[psi+1, psi+i](.py format)为左子树部分; 中序遍历中,[ini, isi+i]为左子树部分
@@ -33,20 +36,22 @@ BinTree ConstructBinTree(char preS[], psi, inS[], isi, num)
 }
 
 // 2 由中序序列和层序序列 构造二叉树
-BinTree ConstructBinTree(leverlS, lsi, inS, isi, num)
+BinTree ConstructBinTreeLI(char leverlS[],int lsi,char inS[],int isi,int num)
 {
+	BinTree T;
 	if(num == 0)	return NULL;
+
 	else if(num == 1)
 	{
-		T = (BinTree)malloc(sizeof(TreeNode));
-		T->data = preS[psi]; T->Left = T->Right = NULL;
+		T = (BinTree)malloc(sizeof(struct TreeNode));
+		T->Data = preS[psi]; T->Left = T->Right = NULL;
 		return T;
 	}
 	else
-		for(int i=0; i < num; i++)
-			if(inS[isi + i] == leverlS[lsi])
-				break
-		T = (BinTree)malloc(sizeof(TreeNode));
+	{	for(int i=0; i < num; i++)
+				if(inS[isi + i] == leverlS[lsi])
+					break
+		T = (BinTree)malloc(sizeof(struct TreeNode));
 		T->Data = leverlS[lsi];
 		// rearrange the leverS order 
 		int j1, j2; j1=j2=0; j2++;
@@ -66,7 +71,7 @@ BinTree ConstructBinTree(leverlS, lsi, inS, isi, num)
 
 		T->Left = ConstructBinTree(leverS, lsi+1, inS, isi, i);
 		T->Right = ConstructBinTree(leverS, lsi+i+1, inS, isi+i+1, num-1-i);
-
+	}
 }
 
 bool InSequence(obj, S, si, num)
